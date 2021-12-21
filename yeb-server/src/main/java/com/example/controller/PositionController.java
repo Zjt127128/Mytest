ec2b122f6d5106eb23e032eca615ee51adeb0a91
zhangjt <1271285066@qq.com>
2021-12-21 13:31:17 +0800

    第三次提交，实现增删改查功能

--- yeb-server/src/main/java/com/example/controller/PositionController.java
+++ yeb-server/src/main/java/com/example/controller/PositionController.java
@@ -23,4 +23,49 @@ import java.util.List;
 @RestController
 @RequestMapping("/system/config/pos")
 public class PositionController {
+    @Autowired
+    private IPositionService positionService;
+    @ApiOperation("获取职位信息")
+    @GetMapping("/")
+    public List<Position> getPosition(){
+        List<Position> list = positionService.list();
+        return list;
+    }
+
+    @ApiOperation("增加职位信息")
+    @PostMapping("/")
+    public RespBean addPosition(@RequestBody Position position){
+        position.setCreateDate(LocalDateTime.now());
+        if(positionService.save(position)){
+            return RespBean.success("添加成功");
+        }
+        return RespBean.error("添加失败");
+    }
+
+    @ApiOperation("更新职位信息")
+    @PutMapping("/")
+    public RespBean updatePosition(@RequestBody Position position){
+        if(positionService.updateById(position)){
+            return RespBean.success("更新成功");
+        }
+        return RespBean.error("更新失败");
+    }
+
+    @ApiOperation("删除职位信息")
+    @DeleteMapping ("/{id}")
+    public RespBean deletePosition(@PathVariable Integer id){
+        if(positionService.removeById(id)){
+            return RespBean.success("删除成功");
+        }
+        return RespBean.error("删除失败");
+    }
+
+    @ApiOperation("批量删除职位信息")
+    @DeleteMapping("/")
+    public RespBean deletePositions(Integer[] ids){
+        if(positionService.removeByIds(Arrays.asList(ids))){
+            return RespBean.success("批量删除成功");
+        }
+        return RespBean.error("批量删除失败");
+    }
 }
